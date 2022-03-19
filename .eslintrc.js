0271a2f3c4de473429834ee4058c1c63186db10d
Luckshya <luckshyaverma@gmail.com>
2022-03-19 16:56:43 +0530

    update tab width for prettier and new npm packages

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      1,
+      {
+        trailingComma: "es5",
+        singleQuote: true,
+        semi: false,
+        tabWidth: 4,
+      },
+    ],
+    ...require("eslint-config-prettier").rules,
+    ...require("eslint-config-prettier/@typescript-eslint").rules,
+  },
+};
