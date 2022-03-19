b1cbdf568112d716c61b08f63ebd355c316e485f
Maan2003 <manmeetmann2003@gmail.com>
2022-03-19 21:18:37 +0530

    test generate: filter to only freeze status

--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -3,8 +3,11 @@ import { Question } from "../models/Question"
 import { handleError } from "./util";
 
 async function generate(req: Request, res: Response) {
-    const questions = await Question.aggregate([{ $sample: { size: count } }]).exec();
     const count = parseInt(req.query.count as string) || 10;
+    const questions = await Question.aggregate([
+        { $match: { status: "freeze" } },
+        { $sample: { size: count } }
+    ]).exec();
     res.json(questions);
 }
 
