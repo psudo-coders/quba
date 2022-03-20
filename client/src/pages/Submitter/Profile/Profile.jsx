e03fe97fe8e469faec12206aa055ecf3749c9398
Lovesh Verma <vermalovesh2002@gmail.com>
2022-03-20 22:14:18 +0530

    add profile section

---
+++ client/src/pages/Submitter/Profile/Profile.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+import "./Profile.css";
+import Page from "../../../components/Page/Page";
+import { FaPlus } from "react-icons/fa";
+import ProfileInfo from "./ProfileInfo";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function Profile(props) {
+    const sidebarOptions = [
+        {
+            label: "Submit Question",
+            highlighted: true,
+            icon: <FaPlus />,
+            link: "",
+        },
+        {
+            label: "Your Questions",
+            link: "",
+        },
+    ];
+
+    const subjectData = {
+        labels: [
+            "Maths",
+            "English",
+            "Hindi",
+            "Science",
+            "Subject3",
+            "Subject4",
+        ],
+        datasets: [
+            {
+                label: "# of Votes",
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    return (
+        <Page sidebarOptions={sidebarOptions} heading={"User Profile"}>
+            <ProfileInfo />
+            <div className={"profile-stats"}>
+                <div className={"question-submitted"}>
+                    <h3>Total Questions Submitted: 82</h3>
+                    <Pie data={subjectData} />
+                </div>
+                <div className={"approve-score"}>
+                    <h3>Approve: 80%</h3>
+                </div>
+            </div>
+        </Page>
+    );
+}
+
+export default Profile;
--- client/src/pages/Submitter/YourQuestions.jsx
+++ client/src/pages/Submitter/YourQuestions.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Page from "../../components/Page/Page";
 import TableRow from "../../components/Table/TableRow";
 import Table from "../../components/Table/Table";
@@ -8,15 +8,30 @@ import TableBody from "../../components/Table/TableBody";
 import { FiFile } from "react-icons/fi";
 import Button from "../../components/Inputs/Button";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Dropdown from "../../components/Dropdown/Dropdown";
 
 function YourQuestions(props) {
     const { sidebarOptions } = props;
+
+    const [selectedStatus, setSelectedStatus] = useState(-1);
+
+    const statusOptions = ["Option 1", "Option 2"];
 
     return (
         <Page
             sidebarOptions={sidebarOptions}
             heading={"Your Questions"}
             subHeading={"Your question description"}
+            search={<SearchBar placeholder={"Search Question"} />}
+            dropdowns={
+                <Dropdown
+                    name={"Status"}
+                    options={statusOptions}
+                    selected={selectedStatus}
+                    setSelected={setSelectedStatus}
+                />
+            }
         >
             <Table>
                 <TableHead>
