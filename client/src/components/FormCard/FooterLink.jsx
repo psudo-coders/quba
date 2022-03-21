2ae55091033604478521f3ca00a098e3ba24dc94
Maan2003 <manmeetmann2003@gmail.com>
2022-03-21 23:43:00 +0530

    Make links work

--- client/src/components/FormCard/FooterLink.jsx
+++ client/src/components/FormCard/FooterLink.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function FooterLink({ label }) {
-    return <p className={"footer-link"}>{label}</p>;
+function FooterLink({ label, link }) {
+    const goto = useNavigate();
+    return <p className={"footer-link"} onClick={() => goto(link)}>{label}</p>;
 }
 
 export default FooterLink;
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -4,6 +4,7 @@ import Button from "../Inputs/Button";
 import SidebarOption from "./SidebarOption";
 
 import "./Sidebar.css";
+import { useNavigate } from "react-router-dom";
 
 const footerOptions = [
     {
@@ -18,6 +19,7 @@ const footerOptions = [
 
 function Sidebar(props) {
     const { options, selected } = props;
+    const goto = useNavigate();
 
     return (
         <div className={"sidebar"}>
@@ -30,17 +32,21 @@ function Sidebar(props) {
                             key={i}
                             label={hOption.label}
                             icon={hOption.icon}
+                            onClick={() => goto(hOption.link)}
                         />
                     ))}
             </div>
             <div className="sidebar-options">
-                {options.map((option, i) => (
-                    <SidebarOption
-                        key={i}
-                        label={option.label}
-                        isSelected={selected === i}
-                    />
-                ))}
+                {options
+                    .filter(o => !o.highlighted)
+                    .map((option, i) => (
+                        <SidebarOption
+                            key={i}
+                            label={option.label}
+                            isSelected={selected === i}
+                            link={option.link}
+                        />
+                    ))}
             </div>
 
             <div className="sidebar-options sidebar-footer-opts">
@@ -49,6 +55,7 @@ function Sidebar(props) {
                         key={i}
                         label={option.label}
                         isSelected={selected === i}
+                        link={option.link}
                     />
                 ))}
             </div>
