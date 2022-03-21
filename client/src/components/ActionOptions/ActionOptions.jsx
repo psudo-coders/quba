b57531f2da71bc5ddf11d9cda4a59f1baab19b9a
Lovesh Verma <vermalovesh2002@gmail.com>
2022-03-21 22:05:17 +0530

    add required components

---
+++ client/src/components/ActionOptions/ActionOptions.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import "./ActionOptions.css";
+import { BsNodePlusFill, BsThreeDots } from "react-icons/bs";
+import { AiFillEdit, AiFillMinusCircle } from "react-icons/ai";
+
+function ActionOptions(props) {
+    const { onEdit, onFreeze, onRemove } = props;
+
+    const [open, setOpen] = useState(false);
+
+    const toggle = () => {
+        setOpen((prev) => !prev);
+    };
+
+    return (
+        <div className={"action-options-container"}>
+            <BsThreeDots onClick={toggle} />
+            {open && (
+                <div className={"action-options"}>
+                    {onEdit && (
+                        <div className={"action"}>
+                            <AiFillEdit color={"blue"} size={20} /> Edit
+                        </div>
+                    )}
+                    {onFreeze && (
+                        <div className={"action"}>
+                            <BsNodePlusFill color={"green"} size={20} /> Freeze
+                        </div>
+                    )}
+                    {onRemove && (
+                        <div className={"action"}>
+                            <AiFillMinusCircle color={"red"} size={20} /> Remove
+                        </div>
+                    )}
+                </div>
+            )}
+        </div>
+    );
+}
+
+ActionOptions.defaultProps = {
+    edit: false,
+    freeze: false,
+    remove: false,
+};
+
+export default ActionOptions;
