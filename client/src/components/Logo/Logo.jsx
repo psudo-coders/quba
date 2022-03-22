c8c1dbd4e00e853b39eec4846bb59145d13bf76c
Luckshya <luckshyaverma@gmail.com>
2022-03-22 20:08:44 +0530

    make logo clickable

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { FiFile } from "react-icons/fi";
 
 import "./Logo.css";
+import { useNavigate } from "react-router-dom";
 
 function Logo(props) {
     const { name, alt } = props;
 
+    const goto = useNavigate();
+
     return (
-        <div className={`logo${alt ? " alt" : ""}`}>
+        <div className={`logo${alt ? " alt" : ""}`} onClick={() => goto("/")}>
             <FiFile />
             <p>{name || "QUBA"}</p>
         </div>
