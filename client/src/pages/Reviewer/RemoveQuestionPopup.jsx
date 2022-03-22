f182c971e67f9dd9397a3dc9d70adbe15ec06c36
Ritesh Gupta <77925980+ritz-gupta@users.noreply.github.com>
2022-03-22 16:52:13 +0530

    Added Popup Close Button

--- client/src/pages/Reviewer/RemoveQuestionPopup.jsx
+++ client/src/pages/Reviewer/RemoveQuestionPopup.jsx
@@ -9,6 +9,7 @@ function RemoveQuestionPopup(props) {
             heading={"Removing Question"}
             middle={<FaEraser className={"eraser"} />}
             bottom={"Question set to be removed, will be removed in 7 days"}
+            setOpen={props.setOpen}
         />
     );
 }
