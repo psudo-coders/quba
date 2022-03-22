9d7d5e12dca0d5e69b3bd3cc559d88adc916121c
Lovesh Verma <vermalovesh2002@gmail.com>
2022-03-22 18:33:18 +0530

    fix design issues with view question popup

--- client/src/pages/Submitter/ViewQuestionPopup.jsx
+++ client/src/pages/Submitter/ViewQuestionPopup.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import Popup from "../../components/Popup/Popup";
 import AttachTextArea from "../../components/AttachTextArea/AttachTextArea";
 import OptionInput from "../../components/OptionInputs/OptionInput";
+import HighlightSection from "../../components/HighlightSection/HighlightSection";
+
+import "./YourQuestionPopup.css";
+import HightlightDisplay from "../../components/HighlightDisplay/HightlightDisplay";
 
 function ViewQuestionPopup({ setOpen }) {
     return (
-        <Popup setOpen={setOpen}>
-            <div className={"view-question"}>
-                <AttachTextArea
-                    heading={"View Question (#QID)"}
-                    placeholder={"Question statement"}
-                    noAttach
-                />
+        <Popup setOpen={setOpen} heading={"View Question(#QID)"}>
+            <HighlightSection className={"view-question"}>
+                <div className={"question-options"}>
+                    <HightlightDisplay name={"English"} />
+                    <HightlightDisplay name={"Grammar"} />
+                    <HightlightDisplay name={"Hard"} />
+                </div>
+                <AttachTextArea placeholder={"Question statement"} noAttach />
                 {["A", "B", "C", "D"].map((option, i) => (
                     <OptionInput key={i} label={option} noOptions />
                 ))}
@@ -20,7 +25,7 @@ function ViewQuestionPopup({ setOpen }) {
                     placeholder={"Question solution"}
                     noAttach
                 />
-            </div>
+            </HighlightSection>
         </Popup>
     );
 }
